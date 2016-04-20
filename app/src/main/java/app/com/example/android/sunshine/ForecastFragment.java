bb36c6e252d382ab7a7771d19734949ee58d37a3
Don Kim <dkim5@2-pc.gateway.2wire.net>
2016-04-20 17:59:19 -0400

    Display data

--- app/src/main/java/app/com/example/android/sunshine/ForecastFragment.java
+++ app/src/main/java/app/com/example/android/sunshine/ForecastFragment.java
@@ -36,6 +36,8 @@ import java.util.List;
  */
 public class ForecastFragment extends Fragment {
 
+    private ArrayAdapter<String> mForecastAdapter;
+    
     public ForecastFragment() {
     }
 
@@ -293,6 +295,17 @@ public class ForecastFragment extends Fragment {
 
             // This will only happen if there was an error getting or parsing the forecast.
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                mForecastAdapter.clear();
+                for(String dayForecastStr : result) {
+                    mForecastAdapter.add(dayForecastStr);
+                    }
+                // New data is back from the server.  Hooray!
+            }
         }
     }
 }
